08f924b0d6ae94f2b0bf219193a91f446c497ec8
me1lof <melissaochoaf04@gmail.com>
2024-11-01 09:28:12 -0700

    initial commit

---
+++ lab26/lab26/lab26.cpp
@@ -0,0 +1,15 @@
+// Melissa Ochoa Flores
+// COMSC 210
+// Data structures race 
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+vector<string> loadData(const string& filename) {
+	vector<string> data;
+	ifstream file
+}
