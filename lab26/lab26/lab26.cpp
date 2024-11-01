5095376112e7b25b6ab4b4f18adc2661fc84aa17
me1lof <melissaochoaf04@gmail.com>
2024-11-01 09:37:54 -0700

    commit #2

--- lab26/lab26/lab26.cpp
+++ lab26/lab26/lab26.cpp
@@ -11,5 +11,16 @@ using namespace std;
 
 vector<string> loadData(const string& filename) {
 	vector<string> data;
-	ifstream file
+	ifstream file(filename);
+	string line;
+	while (getline(file, line)) {
+		data.push_back(line);
+	}
+}
+
+
+int main() {
+	string filename = "codes.txt";
+	vector<string> data = loadData(filename);
+	cout << "Data loaded with size: " << data.size() << endl;
 }
